d723e14fd72e08bcd1f9de5e374acae56473d6d0
Tuan Luu <tuan.luu@asnet.com.vn>
2022-05-11 22:28:33 +0700

    Init project

---
+++ apps/bookstore/src/app/app.spec.tsx
@@ -0,0 +1,19 @@
+import { render, cleanup } from '@testing-library/react';
+
+import App from './app';
+
+describe('App', () => {
+  afterEach(cleanup);
+
+  it('should render successfully', () => {
+    const { baseElement } = render(<App />);
+
+    expect(baseElement).toBeTruthy();
+  });
+
+  it('should have a header as the title', () => {
+    const { getByText } = render(<App />);
+
+    expect(getByText('Bookstore')).toBeTruthy();
+  });
+});
---
+++ apps/bookstore/src/app/app.tsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const StyledApp = styled.div``;
+
+export const App = () => {
+  return (
+    <StyledApp>
+      <header>
+        <h1>Bookstore</h1>
+      </header>
+    </StyledApp>
+  );
+};
+
+export default App;
