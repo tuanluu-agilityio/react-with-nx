5a23c8a8a4549771888acf6bbbeb38d4f2267dde
Tuan Luu <tuan.luu@asnet.com.vn>
2022-05-16 11:54:04 +0700

    Added api app

---
+++ apps/api/src/main.ts
@@ -0,0 +1,60 @@
+/**
+ * This is not a production server yet!
+ * This is only a minimal backend to get started.
+ */
+
+import * as express from 'express';
+
+const app = express();
+
+app.get('/api', (req, res) => {
+  res.send({ message: 'Welcome to api!' });
+});
+
+app.get('/api/books', (req, res) => {
+  const books: any[] = [
+    {
+      id: 1,
+      title: 'The Picture of Dorian Gray',
+      author: 'Oscar Wilde',
+      rating: 3,
+      price: 9.99
+    },
+    {
+      id: 2,
+      title: 'Frankenstein',
+      author: 'Mary Wollstonecraft Shelley',
+      rating: 5,
+      price: 7.95
+    },
+    {
+      id: 3,
+      title: 'Jane Eyre',
+      author: 'Charlotte Brontë',
+      rating: 4,
+      price: 10.95
+    },
+    {
+      id: 4,
+      title: 'Dracula',
+      author: 'Bram Stoker',
+      rating: 5,
+      price: 14.99
+    },
+    {
+      id: 5,
+      title: 'Pride and Prejudice',
+      rating: 4,
+      author: 'Jane Austen',
+      price: 12.85
+    }
+  ];
+
+  res.send(books);
+});
+
+const port = process.env.port || 3333;
+const server = app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}/api`);
+});
+server.on('error', console.error);
